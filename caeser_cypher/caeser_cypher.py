95f66bf609cd41a0aa4112ca74b7304c4cdf1dc0
KimrAndrew <andrewrk1997@gmail.com>
2021-12-26 18:07:31 -0800

    set up corpus, added functions for determining validity of a phrase

--- caeser_cypher/caeser_cypher.py
+++ caeser_cypher/caeser_cypher.py
@@ -1,15 +1,23 @@
+import nltk
+
+nltk.download('words', quiet = True)
+nltk.download('names', quiet = True)
+
+from nltk.corpus import words, names
+
+word_list = words.words()
+name_list = names.words()
+import re
+
 def shift_char(char,shift):
     char_code = ord(char)
     if ord('A') <= char_code <= ord('Z'):
         shifted_code = char_code + shift
         if shifted_code > ord('Z'):
-            #wrap = shifted_code % ord('Z')
             wrap = shifted_code % ord('Z')
             shifted_code = ord('A') + wrap
         if shifted_code < ord('A'):
             wrap = 1 + (ord('A') % shifted_code) * -1
-            #print('SHIFT:',shifted_code)
-            #print('WRAP:',wrap)
             shifted_code = ord('Z') + wrap
         shifted_char = chr(shifted_code)
         return shifted_char
@@ -33,23 +41,6 @@ def encrypt(plain,shift):
         encrypted += shift_char(char,shift)
     return encrypted
 
-    # for char in plain:
-        
-    #     # assigned to None if  not upper
-    #     encrypted_char = _shift(char,shift,(ord('A'),ord('B')))
-
-    #     # assigned to None if not lower after checking if upper
-    #     if not encrypted_char:
-    #         encrypted_char = _shift(char,shift,(ord('a'),ord('b')))
-
-    #     # if neither, do nothing - return char as is
-    #     if not encrypted_char:
-    #         encrypted_char = char
-
-    #     encrypted += encrypted_char
-    
-    # return encrypted 
-
 def decrypt(encrypted,shift):
     return encrypt(encrypted, -shift)
 
@@ -58,6 +49,33 @@ def crack(encrypted):
     for shift in range(26):
         cracked.append(decrypt(encrypted,shift))
     return cracked
+
+def get_valid_words(candidate_phrase):
+    words = []
+    for candidate_word in candidate_phrase:
+        candidate_word = re.sub(r'[^a-zA-Z]+')
+        if candidate_word.lower() in word_list or candidate_word in name_list:
+            words.append(candidate_word)
+    return words
+
+def get_phrase_validity_percentage(candidate_phrase):
+    denominator = len(candidate_phrase.split())
+    nummerator = len(get_valid_words(candidate_phrase))
+    percentage = int(nummerator / denominator) * 100
+    return percentage
+
+def get_most_valid_phrase(candidate_phrases):
+    candidate_percentages = {}
+    for candidate_phrase in candidate_phrases:
+        candidate_percentages[candidate_phrase] = get_phrase_validity_percentage(candidate_phrase)
+    most_valid = ''
+    for candidate_phrase in candidate_percentages:
+        if candidate_percentages.get(candidate_phrase) > candidate_percentages.get(most_valid):
+            most_valid = candidate_phrase
+    return max
+        
+            
+
 
 if __name__ == '__main__':
     print(shift_char('H',26))
